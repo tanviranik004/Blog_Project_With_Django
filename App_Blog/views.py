b4e1e19f5aef037b3d810571273114ebef49ff60
Tanvir <tanvir.anik004@gmail.com>
2023-01-21 23:32:59 +0600

    comment

--- App_Blog/views.py
+++ App_Blog/views.py
@@ -9,7 +9,8 @@ from django.urls import reverse, reverse_lazy
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
 import uuid
-from App_Blog.forms import CommentForm
+from App_Blog.forms import CommentForm 
+from .models import Blog
 # from django.shortcuts import get_object_or_404
 # Create your views here.
 
@@ -22,6 +23,7 @@ class CreateBlog(LoginRequiredMixin, CreateView):
     template_name = 'App_Blog/create_blog.html'
     fields = ('blog_title', 'blog_content', 'blog_image',)
 
+
     def form_valid(self, form):
         blog_obj = form.save(commit=False)
         blog_obj.author = self.request.user
@@ -29,6 +31,13 @@ class CreateBlog(LoginRequiredMixin, CreateView):
         blog_obj.slug = title.replace(" ","-") + "-" + str(uuid.uuid4())
         blog_obj.save()
         return HttpResponseRedirect(reverse('Index'))
+
+
+
+
+
+
+
 
 class BlogList(ListView):
     context_object_name='blogs'
@@ -40,11 +49,19 @@ class BlogList(ListView):
 @login_required
 def blog_details(request, slug):
     blog =Blog.objects.get(id=slug)
-    # all_comments = BlogComment.objects.filter(blog = blog.id)
-    # blog = Blog.objects.filter(blog=blog_id)
     all_blogs = Blog.objects.all().order_by('-publish_date')[:10]
+    comment_form = CommentForm()
 
-    return render(request, 'App_Blog/blog_details.html',context={'blog':blog})
+    if request.method == 'POST':
+         comment_form = CommentForm(request.POST)
+         if comment_form.is_valid():
+            comment = comment_form.save(commit=False)
+            comment.user = request.user
+            comment.blog = blog
+            comment.save()
+            return HttpResponseRedirect(reverse('App_Blog:blog_details', kwargs={'slug':slug}))
+
+    return render(request, 'App_Blog/blog_details.html',context={'blog':blog, 'comment_form':comment_form})
 
 
 
